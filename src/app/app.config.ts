b78d14596ef202f13867f5454eb9f879b42edd3d
Mathis <mathis@wesharetrust.com>
2025-08-10 22:49:27 +0200

    feat: design system for text, color, input and buttons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,29 @@ import { type ApplicationConfig, provideBrowserGlobalErrorListeners, provideZone
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeuix/themes/aura";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // PrimeNG
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: "primeng",
+            order: "primeng, mytheme"
+          }
+        }
+      }
+    })
   ]
 };
